ef366ae79b46f5e7e56cb96a3a91741377a64500
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2015-07-06 11:10:02 +0200

    fix: check result type

--- src/osdbinfos/osdbinfos.py
+++ src/osdbinfos/osdbinfos.py
@@ -196,7 +196,8 @@ class OpenSutitles(object):
             elif isinstance(datas, list):
                 # osdb gave us a list, which is not what is expected.
                 # transform the list in a dictionary and parse it as expected
-                datas = {x['MovieHash']: x for x in datas}
+                # we check the type of items
+                datas = {x['MovieHash']: x for x in datas if isinstance(x, dict)}
                 return self._parse_dict(datas)
 
     def _parse_dict(self, datas):
